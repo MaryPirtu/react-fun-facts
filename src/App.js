dad3868f221695babd842a16189781b41178572c
MaryPirtu <maria.pirtu@hotmail.com>
2023-03-12 12:12:48 +0000

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import logo from "./logo.png"
 
 
 export default function App() {
   return (
     <div className="App">
-
+      <img src={logo} alt="react-logo" width={"60px"} />
     </div>
   );
 }
