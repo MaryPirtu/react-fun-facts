93608f4765553e4c1867bffb855e83f81fde855a
MaryPirtu <maria.pirtu@hotmail.com>
2023-03-12 12:16:29 +0000

    Add facts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ export default function App() {
   return (
     <div className="App">
       <img src={logo} alt="react-logo" width={"60px"} />
+      <h1>Reasons I like React</h1>
+      <ol>
+        <li>it's composable</li>
+        <li>it's declarativd </li>
+        <li>it's a hearable skill</li>
+        <li>it's actively maintained by skilled people</li>
+      </ol>
     </div>
   );
 }
